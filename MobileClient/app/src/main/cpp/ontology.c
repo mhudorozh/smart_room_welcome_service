b4cbbebf4e82fb2d3d4bd04cb6a0e3cdc2948531
gdhsnlvr <gdhsnlvr@yandex.ru>
2017-04-04 14:11:58 +0300

    welcome activity

--- MobileClient/app/src/main/cpp/ontology.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by gdhsnlvr on 03.04.17.
-//
-
-#include "ontology.h"
--- MobileClient/app/src/main/cpp/ontology.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by gdhsnlvr on 03.04.17.
-//
-
-#ifndef MOBILECLIENT_ONTOLOGY_H
-#define MOBILECLIENT_ONTOLOGY_H
-
-#endif //MOBILECLIENT_ONTOLOGY_H
